9556f3524dfd407aaf502df494b23261636ebd99
m3tro1d <qazwf734@gmail.com>
2020-06-03 20:36:45 +0300

    Add an argument parser

--- pybatch.py
+++ pybatch.py
@@ -4,6 +4,11 @@ import argparse
 import shutil
 from glob import glob
 
-# Check the input parameters (pattern, directory)
+# Parse the input parameters
+parser = argparse.ArgumentParser(
+	description='''Renames all files in the directory that match the pattern with
+[pseudo] randomly generated names.''')
+
+args = parser.parse_args()
 
 # Rename the files (only random names for now)
