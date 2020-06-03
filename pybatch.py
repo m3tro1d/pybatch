32d991b8e3c922c7948a195452ce1791fd3da762
m3tro1d <qazwf734@gmail.com>
2020-06-03 20:42:01 +0300

    Add the directory argument

--- pybatch.py
+++ pybatch.py
@@ -9,6 +9,10 @@ parser = argparse.ArgumentParser(
 	description='''Renames all files in the directory that match the pattern with
 [pseudo] randomly generated names.''')
 
+parser.add_argument('-d', '--dir', default='.',
+	help='Files directory. The default is current directory.')
+
 args = parser.parse_args()
+print(args)
 
 # Rename the files (only random names for now)
